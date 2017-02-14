0166e34e605c1a51090c9456364b44ad2aed3cf8
ImFalling <jakobdoesnotapproveofschool@gmail.com>
2017-02-14 17:51:07 +0100

    Added a dynamic header

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,16 @@ window.addEventListener("DOMContentLoaded", function(e){
     window.requestAnimationFrame(responsiveUpdates);
 }, false);
 
+var elementCreated = false;
+var temp;
 function responsiveUpdates(){
-
+    if(window.scrollY >= (document.getElementById("header").offsetTop)){
+        document.getElementById("header").style.opacity = "0";
+        document.getElementById("movingheader").classList.add("movingHeader");
+    }
+    else{
+        document.getElementById("header").style.opacity = "1";
+        document.getElementById("movingheader").classList.remove("movingHeader");
+    }
+    window.requestAnimationFrame(responsiveUpdates);
 }
